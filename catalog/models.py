344db929830d736384c1ba515315c0f9b8b02f13
Viktor Nikulaev <viktor_1611@mail.ru>
2024-08-30 23:56:35 +0300

    create migrations

--- catalog/models.py
+++ catalog/models.py
@@ -20,17 +20,17 @@ class Category(models.Model):
 
 class Product(models.Model):
     """Модель продукта"""
-    title = models.CharField(max_lenght=200, verbose_name='Название товара',
+    title = models.CharField(max_length=200, verbose_name='Название товара',
                              help_text="Введите наименование продукта", )
     description = models.TextField(verbose_name='Описание продукта', help_text="Введите описание продукта", )
     image = models.ImageField(upload_to='products/', verbose_name='Изображение(превью)',
                               help_text='Вставьте изображение продукта', **NULLABLE, )
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,
-                                 verbose_name='Категория', help_text= 'Введите категорию', **NULLABLE,
+                                 verbose_name='Категория', help_text='Введите категорию', **NULLABLE,
                                  related_name='products', )
     price = models.IntegerField(verbose_name='Цена, за покупку')
-    created_at = models.DateTimeField(verbose_name='Дата создания записи в БД', **NULLABLE)
-    updated_at = models.DateTimeField(verbose_name='Дата последнего изменения записи в БД')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания записи в БД', **NULLABLE)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения записи в БД')
 
     def __str__(self):
         return f'{self.title} {self.description} {self.price}'
--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postresql',
-        'NAME': 'my_datebase',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatebase',
         'USER': 'postgres',
         'PASSWORD': 'TheBmW525760'
     }
@@ -121,6 +121,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (BASE_DIR / 'static',)
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR / "media", )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
