aa2fc844e473f42b71761dbfaa31ec959caabf9a
Viktor Nikulaev <viktor_1611@mail.ru>
2024-09-04 19:28:01 +0300

    add product_list, product_detail, my_tags

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from catalog.views import home, contacts
+from catalog.views import contacts, product_list, product_detail, base
+from catalog.apps import CatalogConfig
 
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
+    # path('', home, name='home'),
+    path('', base, name='base'),
+    path('product_list/', product_list, name='product_list'),
+    path('products/<int:pk>/', product_detail, name='product_detail'),
+    path('contacts/', contacts, name='contacts')
 ]
