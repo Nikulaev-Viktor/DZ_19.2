9717d19c1e5fcde9a0a465cb3bf2e2f580300c2f
Viktor Nikulaev <viktor_1611@mail.ru>
2024-09-07 18:03:09 +0300

    правки по замечаниям

--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR / "media", )
+MEDIA_ROOT = os.path.join(BASE_DIR / "media",)
 
 # STATIC_URL = '/static/'
 #
